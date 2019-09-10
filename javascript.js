6c56d1e2d49e75e10c43140d3d9dcc58e4255644
Joshua Einsel <Joshua.einsel@live.com>
2019-09-10 09:38:33 -0700

    Javascript done, need to work on final function.

---
+++ javascript.js
@@ -0,0 +1,93 @@
+var randomNumber = document.getElementById("randomNumber")
+
+var randomMin = 20;
+
+var randomMax = 50;
+
+var min = 1;
+
+var max = 10;
+
+var wins = 0;
+
+var losses = 0;
+
+var score = 0;
+
+var button1 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+var button2 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+var button3 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+var button4 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+
+var random = Math.floor(Math.random() * (+randomMax - +randomMin)) + +min;
+randomNumber.innerHTML = random
+
+
+function update() {
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("score").innerHTML = score;
+}
+update()
+
+
+function newGame(){
+    var random = Math.floor(Math.random() * (+randomMax - +randomMin)) + +min;
+randomNumber.innerHTML = random
+
+score = 0;
+
+button1 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+button2 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+button3 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+button4 = Math.floor(Math.random() * (+max - +min)) + +min;
+
+update()
+}
+
+function winLoss() {
+    if (score === random) {
+        alert("You win!")
+        wins++
+        newGame()
+        update()
+    } else if (score > random) {
+        alert("You lose.")
+        losses++
+        newGame()
+        update()
+    }
+
+}
+
+$('#button1').click(function () {
+    console.log(button1)
+    score = score + button1
+    update()
+    winLoss()
+});
+$('#button2').click(function () {
+    console.log(button2)
+    score = score + button2;
+    update()
+    winLoss()
+});
+$('#button3').click(function () {
+    console.log(button3)
+    score = score + button3;
+    update()
+    winLoss()
+});
+$('#button4').click(function () {
+    console.log(button4)
+    score = score + button4;
+    update()
+    winLoss()
+})
